f9fff72fc215e01147315db8b6cc36b76c70406f
Adriano Bisca <adrianobisca@gmail.com>
2022-03-05 19:09:54 -0300

    feat: core layout implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { PokemonListComponent } from './pokemons/pokemon-list/pokemon-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pokemon-list', component: PokemonListComponent },
+  { path: '', redirectTo: '/pokemon-list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './pokemons/pokemon/pokemon.component';
+import { PokemonListComponent } from './pokemons/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemons/pokemon-detail/pokemon-detail.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent,
+    PokemonListComponent,
+    PokemonDetailComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
