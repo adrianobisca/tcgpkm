bc0d18f610c5f8a141da9831b0268a3e5d14d94d
adrianobisca <adrianobisca@gmail.com>
2024-08-25 21:18:36 -0300

    chore: include speed insight tool from vercel

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err))
+  .finally(() => {
+    console.log('Vercel Speed Insight & Analytics Activated');
+    injectSpeedInsights();
+  });
